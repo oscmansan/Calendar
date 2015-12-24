159b4fb98ac5dfa384a060d22e0cc68df4869720
Oscar Mañas <oscar.manas@est.fib.upc.edu>
2015-12-24 14:00:23 +0100

    Some layout changes

--- app/src/main/java/oscmansan/calendar/DayAdapter.java
+++ app/src/main/java/oscmansan/calendar/DayAdapter.java
@@ -5,25 +5,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class DayAdapter extends ArrayAdapter<Integer> {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class DayAdapter extends ArrayAdapter<Calendar> {
+
+    private static final String LOG_TAG = DayAdapter.class.getSimpleName();
 
     private Context context;
-    private Integer[] days_of_month;
+    private ArrayList<Calendar> days;
 
-    public DayAdapter(Context context, Integer[] days) {
+    public DayAdapter(Context context, ArrayList<Calendar> days) {
         super(context, -1, days);
 
         this.context = context;
-        this.days_of_month = days;
+        this.days = days;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.day, parent, false);
+        View view = convertView;
+        if (view == null)
+            view = LayoutInflater.from(context).inflate(R.layout.day, parent, false);
 
-        ((TextView)view.findViewById(R.id.day_of_month)).setText(String.valueOf(days_of_month[position]));
+        int day_of_month = days.get(position).get(Calendar.DAY_OF_MONTH);
+        String day_of_week = days.get(position).getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US);
+        ((TextView)view.findViewById(R.id.day_of_month)).setText(String.valueOf(day_of_month));
+        ((TextView)view.findViewById(R.id.day_of_week)).setText(day_of_week);
+
+        LinearLayout eventList = (LinearLayout)view.findViewById(R.id.events);
+        eventList.removeAllViews();
+        String[] events = {"Event 1", "Event 2", "Event 3"};
+        for (String s : events) {
+            View event = LayoutInflater.from(context).inflate(R.layout.event, parent, false);
+            ((TextView)event.findViewById(R.id.event)).setText(s);
+            eventList.addView(event);
+        }
 
         return view;
     }
--- app/src/main/java/oscmansan/calendar/MainActivity.java
+++ app/src/main/java/oscmansan/calendar/MainActivity.java
@@ -1,118 +1,40 @@
 package oscmansan.calendar;
 
-import android.content.AsyncQueryHandler;
-import android.content.ContentResolver;
-import android.content.ContentUris;
-import android.content.ContentValues;
-import android.database.Cursor;
-import android.net.Uri;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.provider.CalendarContract;
-import android.provider.CalendarContract.Calendars;
-import android.provider.CalendarContract.Events;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
-    private ContentResolver cr;
-    private MyQueryHandler queryHandler;
-
-    //private ListView listView;
-    //private EventAdapter adapter;
-    private Button insert_button;
-    private Button delete_button;
-    private long calID;
-    private DatePicker datePicker;
     private ListView week;
-    private DayAdapter dayAdapter;
+    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cr = getContentResolver();
-        queryHandler = new MyQueryHandler(cr);
-
-        /*listView = (ListView)findViewById(R.id.list);
-        adapter = new EventAdapter(this);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(adapter);*/
-
         week = (ListView)findViewById(R.id.week);
+        title = (TextView)findViewById(R.id.title);
 
         Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
-        int day = c.get(Calendar.DAY_OF_MONTH);
-
-        datePicker = (DatePicker)findViewById(R.id.date_picker);
-        datePicker.init(year, month, day, dateSetListener);
-
-        Calendar date = Calendar.getInstance();
-        date.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-        int d = date.get(Calendar.DAY_OF_MONTH);
-        Integer[] days_of_month = {d,d+1,d+2,d+3,d+4,d+5,d+6};
-        dayAdapter = new DayAdapter(this,days_of_month);
-        week.setAdapter(dayAdapter);
-
-        insert_button = (Button)findViewById(R.id.insert_button);
-        insert_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //insertEvent();
-            }
-        });
-
-        delete_button = (Button)findViewById(R.id.delete_button);
-        delete_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //deleteAllEvents();
-            }
-        });
-
-        if (getCalendarID() == -1)
-            insertCalendar();
-        calID = getCalendarID();
+        showWeek(c);
     }
-
-    private DatePicker.OnDateChangedListener dateSetListener = new DatePicker.OnDateChangedListener() {
-
-        public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            Log.d(LOG_TAG, "year: " + year + "  month: " + monthOfYear + "  day: " + dayOfMonth);
-            Calendar c = Calendar.getInstance();
-            c.set(year, monthOfYear, dayOfMonth);
-
-            int day_of_week = c.get(Calendar.DAY_OF_WEEK);
-            c.roll(Calendar.DAY_OF_WEEK,-(day_of_week-Calendar.MONDAY));
-            int d = c.get(Calendar.DAY_OF_MONTH);
-            Integer[] days_of_month = {d,d+1,d+2,d+3,d+4,d+5,d+6};
-            DayAdapter dayAdapter = new DayAdapter(MainActivity.this,days_of_month);
-            dayAdapter.notifyDataSetChanged();
-            week.setAdapter(dayAdapter);
-        }
-    };
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        //showCalendars();
-        //showEvents();
     }
 
     @Override
@@ -129,161 +51,48 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+            case R.id.pick_date:
+                pickDate();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
-    // Projection array. Creating indices for this array instead of doing
-    // dynamic lookups improves performance.
-    public static final String[] EVENT_PROJECTION = new String[] {
-            Calendars._ID,                           // 0
-            Calendars.ACCOUNT_NAME,                  // 1
-            Calendars.CALENDAR_DISPLAY_NAME,         // 2
-            Calendars.OWNER_ACCOUNT                  // 3
+    void pickDate() {
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePicker = new DatePickerDialog(this,dateSetListener,year,month,day);
+        datePicker.show();
+    }
+
+    void showWeek(Calendar c) {
+        int day_of_week = c.get(Calendar.DAY_OF_WEEK);
+        c.roll(Calendar.DAY_OF_WEEK, -(day_of_week - Calendar.MONDAY));
+        ArrayList<Calendar> days = new ArrayList<>();
+        for (int i = 0; i < 7; ++i) {
+            days.add((Calendar)c.clone());
+            c.roll(Calendar.DAY_OF_MONTH, 1);
+        }
+        week.setAdapter(new DayAdapter(MainActivity.this, days));
+
+        String s = "Week " + c.get(Calendar.WEEK_OF_YEAR) + " of " + c.get(Calendar.YEAR);
+        title.setText(s);
+    }
+
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            Log.d(LOG_TAG, "year: " + year + "  month: " + monthOfYear + "  day: " + dayOfMonth);
+            Calendar c = Calendar.getInstance();
+            c.set(year, monthOfYear, dayOfMonth);
+            showWeek(c);
+        }
     };
 
-    // The indices for the projection array above.
-    private static final int PROJECTION_ID_INDEX = 0;
-    private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
-    private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
-    private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
-
-    private void showCalendars() {
-        Uri uri = Calendars.CONTENT_URI;
-        // Submit the query and get a Cursor object back.
-        Cursor cur = cr.query(uri, EVENT_PROJECTION, null, null, null);
-        // Use the cursor to step through the returned records
-        while (cur.moveToNext()) {
-            // Get the field values
-            long calID = cur.getLong(PROJECTION_ID_INDEX);
-            String displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
-            String accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
-            String ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
-
-            // Do something with the values...
-            Log.d(LOG_TAG, calID + " \t" + displayName + " \t" + accountName + " \t" + ownerName + "\n\n");
-        }
-        cur.close();
-    }
-
-    private void insertCalendar() {
-        ContentValues values = new ContentValues();
-        values.put(Calendars.ACCOUNT_NAME,"some.account@googlemail.com");
-        values.put(Calendars.ACCOUNT_TYPE, CalendarContract.ACCOUNT_TYPE_LOCAL);
-        values.put(Calendars.NAME,"Local Calendar");
-        values.put(Calendars.CALENDAR_DISPLAY_NAME,"Local Calendar");
-        values.put(Calendars.CALENDAR_COLOR,0xffff0000);
-        values.put(Calendars.CALENDAR_ACCESS_LEVEL,Calendars.CAL_ACCESS_OWNER);
-        values.put(Calendars.OWNER_ACCOUNT,"some.account@googlemail.com");
-        values.put(Calendars.CALENDAR_TIME_ZONE,"Europe/Madrid");
-        values.put(Calendars.SYNC_EVENTS, 1);
-
-        Uri.Builder builder = Calendars.CONTENT_URI.buildUpon();
-        builder.appendQueryParameter(Calendars.ACCOUNT_NAME,"some.account@googlemail.com");
-        builder.appendQueryParameter(Calendars.ACCOUNT_TYPE,CalendarContract.ACCOUNT_TYPE_LOCAL);
-        builder.appendQueryParameter(CalendarContract.CALLER_IS_SYNCADAPTER,"true");
-
-        Uri uri = cr.insert(builder.build(),values);
-        long calendarID = Long.parseLong(uri.getLastPathSegment());
-        Log.d(LOG_TAG, "Calendar inserted: " + calendarID);
-    }
-
-    private void deleteCalendar() {
-        Uri deleteUri = ContentUris.withAppendedId(Calendars.CONTENT_URI, calID);
-        int rows = getContentResolver().delete(deleteUri, null, null);
-        Log.d(LOG_TAG, "Rows deleted: " + rows);
-    }
-
-    private long getCalendarID() {
-        String[] projection = {Calendars._ID};
-        String selection = "((" + Calendars.ACCOUNT_NAME + " = ?) AND ("
-                                + Calendars.ACCOUNT_TYPE + " = ?))";
-        String[] selectionArgs = {
-                "some.account@googlemail.com",
-                CalendarContract.ACCOUNT_TYPE_LOCAL
-        };
-        Cursor cur = cr.query(Calendars.CONTENT_URI, projection, selection, selectionArgs, null);
-
-        long calID = -1;
-        if (cur.moveToNext()) {
-            calID = cur.getLong(0);
-        }
-        cur.close();
-        return calID;
-    }
-
-    private void showEvents() {
-        String[] projection = {Events._ID, Events.TITLE, Events.STATUS};
-        String selection = "(" + Events.CALENDAR_ID + " = ?)";
-        String[] selectionArgs = {String.valueOf(calID)};
-
-        queryHandler.startQuery(1,null,Events.CONTENT_URI,projection,selection,selectionArgs,null);
-    }
-
-    private void insertEvent() {
-        Calendar beginTime = Calendar.getInstance();
-        beginTime.set(2015, 12, 25, 12, 0);
-        long startMillis = beginTime.getTimeInMillis();
-        Calendar endTime = Calendar.getInstance();
-        endTime.set(2015, 12, 25, 13, 0);
-        long endMillis = endTime.getTimeInMillis();
-
-        ContentValues values = new ContentValues();
-        values.put(Events.DTSTART, startMillis);
-        values.put(Events.DTEND, endMillis);
-        values.put(Events.TITLE, "Test Event");
-        values.put(Events.CALENDAR_ID, calID);
-        values.put(Events.EVENT_TIMEZONE, "Europe/Madrid");
-        values.put(Events.STATUS,Events.STATUS_TENTATIVE);
-
-        queryHandler.startInsert(1, null, Events.CONTENT_URI, values);
-    }
-
-    private void deleteEvent(long eventID) {
-        Uri deleteUri = ContentUris.withAppendedId(Events.CONTENT_URI, eventID);
-        queryHandler.startDelete(1, null, deleteUri, null, null);
-    }
-
-    private void deleteAllEvents() {
-        String[] projection = {Events._ID};
-        String selection = "(" + Events.CALENDAR_ID + " = ?)";
-        String[] selectionArgs = {String.valueOf(calID)};
-        Cursor cur = cr.query(Events.CONTENT_URI, projection,selection,selectionArgs,null);
-
-        while (cur.moveToNext()) {
-            long eventID = cur.getLong(0);
-            deleteEvent(eventID);
-        }
-        cur.close();
-    }
-
-    private class MyQueryHandler extends AsyncQueryHandler {
-
-        public MyQueryHandler(ContentResolver cr) {
-            super(cr);
-        }
-
-        @Override
-        protected void onQueryComplete(int token, Object cookie, Cursor cur) {
-            //adapter.swapCursor(cur);
-        }
-
-        @Override
-        protected void onInsertComplete(int token, Object cookie, Uri uri) {
-            // get the event ID that is the last element in the Uri
-            long eventID = Long.parseLong(uri.getLastPathSegment());
-            Log.d(LOG_TAG, "Event inserted: " + eventID);
-            showEvents();
-        }
-
-        @Override
-        protected void onDeleteComplete(int token, Object cookie, int result) {
-            Log.d(LOG_TAG, "Rows deleted: " + result);
-            showEvents();
-        }
-    }
 }
